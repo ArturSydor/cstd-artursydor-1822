ce990dc2b669f875ad69d7960a9efc2f96b78ce2
artursydor <sydor.artur@ukr.net>
2021-11-15 23:15:50 +0200

    CSTD-1 - init projects

---
+++ eco-platform-server/src/main/java/com/lpnu/ecoplatformserver/EcoPlatformServerApplication.java
@@ -0,0 +1,13 @@
+package com.lpnu.ecoplatformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPlatformServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPlatformServerApplication.class, args);
+	}
+
+}
